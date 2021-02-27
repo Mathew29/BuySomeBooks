2769f9e6bd1922fa0360fd431e03ad6a117b800d
Mathew <mwakre29@gmail.com>
2021-02-26 20:57:18 -0800

    add api call

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 const Card = () => {
+    const [bookApi, setBookApi] = useState(initialState)
 
+    useEffect(() => {
+        FakerApi()
+    }, [])
+
+    const FakerApi = () => {
+        fetch('https://fakerapi.it/api/v1/books?_quantity=20').then(response => response.json())
+        .then(data => console.log(data.data));
+    }
+
+
+    return (
+        <div></div>
+    )
 }
 
 export default Card;
