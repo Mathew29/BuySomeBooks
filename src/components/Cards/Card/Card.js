a50d120b78c7d8d9abc87039d417be8e70a971e9
Mathew <mwakre29@gmail.com>
2021-03-02 15:04:50 -0800

    add genre

--- src/components/Cards/Card/Card.js
+++ src/components/Cards/Card/Card.js
@@ -5,7 +5,7 @@ import styles from './Card.module.css';
 
 const Card = (props) => {
 
-    console.log('props ',props)
+    // console.log('props ',props)
     return (
         <div className={styles.Container}>
             <div className={styles.Image}>
@@ -15,6 +15,7 @@ const Card = (props) => {
                 <h3 className={styles.Title}>{props.title}</h3>
                 <p className={styles.Description}>{props.description}</p>
                 <p className={styles.Author}>{props.author}</p>
+                <p className={styles.Genre}>{props.genre}</p>
             </div>
 
         </div>
