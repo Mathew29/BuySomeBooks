e37e89d0a684522f4915e7ec329c851437889c3d
Mathew <mwakre29@gmail.com>
2021-03-02 13:43:38 -0800

    add html

--- src/components/Cards/Card/Card.js
+++ src/components/Cards/Card/Card.js
@@ -1,4 +1,7 @@
+import { StylesProvider } from '@material-ui/core';
 import React from 'react';
+import styles from './Card.module.css';
+
 import Auxiliary from '../../../hoc/Auxiliary/Auxiliary';
 
 
@@ -6,9 +9,16 @@ const Card = (props) => {
 
     console.log('props ',props)
     return (
-        <Auxiliary>
+        <div className={styles.Container}>
+            <div className={styles.Image}>
+                image
+            </div>
+            <div className={styles.Body}>
+                <h3 className={styles.Title}>title</h3>
+                <p className={styles.Description}>description</p>
+            </div>
 
-        </Auxiliary>
+        </div>
     )
 }
 
