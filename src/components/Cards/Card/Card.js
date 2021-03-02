2d93c2416e1f46ec286cc798a066cb96d1520d05
Mathew <mwakre29@gmail.com>
2021-03-02 13:51:38 -0800

    add tags

--- src/components/Cards/Card/Card.js
+++ src/components/Cards/Card/Card.js
@@ -2,8 +2,6 @@ import { StylesProvider } from '@material-ui/core';
 import React from 'react';
 import styles from './Card.module.css';
 
-import Auxiliary from '../../../hoc/Auxiliary/Auxiliary';
-
 
 const Card = (props) => {
 
@@ -11,11 +9,12 @@ const Card = (props) => {
     return (
         <div className={styles.Container}>
             <div className={styles.Image}>
-                image
+                <img src={props.image} />
             </div>
             <div className={styles.Body}>
-                <h3 className={styles.Title}>title</h3>
-                <p className={styles.Description}>description</p>
+                <h3 className={styles.Title}>{props.title}</h3>
+                <p className={styles.Description}>{props.description}</p>
+                <p className={styles.Author}>{props.author}</p>
             </div>
 
         </div>
