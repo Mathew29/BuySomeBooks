28e40759ff2a3686320c3a40b774e8e451c68b38
Mathew <mwakre29@gmail.com>
2021-03-02 18:31:26 -0800

    reduce display

--- src/components/Cards/Card/Card.js
+++ src/components/Cards/Card/Card.js
@@ -1,24 +1,26 @@
 import { StylesProvider } from '@material-ui/core';
 import React from 'react';
 import styles from './Card.module.css';
+import Auxiliary from "../../../hoc/Auxiliary/Auxiliary";
 
 
 const Card = (props) => {
 
     // console.log('props ',props)
     return (
-        <div className={styles.Container}>
+        <Auxiliary>
             <div className={styles.Image}>
                 <img src={props.image} />
             </div>
             <div className={styles.Body}>
-                <h3 className={styles.Title}>{props.title}</h3>
-                <p className={styles.Description}>{props.description}</p>
-                <p className={styles.Author}>{props.author}</p>
-                <p className={styles.Genre}>{props.genre}</p>
+                <h3 className={styles.Title}>title {props.title}</h3>
+                <p className={styles.Price}>price</p>
+                {/* <p className={styles.Description}>{props.description}</p> */}
+                {/* <p className={styles.Author}>{props.author}</p>
+                <p className={styles.Genre}>{props.genre}</p> */}
             </div>
 
-        </div>
+        </Auxiliary>
     )
 }
 
