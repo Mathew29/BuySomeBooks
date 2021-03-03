49c1dcdcc2c6a1b3e93b83e077d1fabfb0af6018
Mathew <mwakre29@gmail.com>
2021-03-02 18:30:35 -0800

    create a card layout

---
+++ src/components/CardLayout/CardLayout.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./CardLayout.module.css";
+import Cards from "../Cards/Cards";
+
+const Layout = () => {
+  return (
+    <div className={styles.container}>
+      <ul className={styles.main}>
+          <Cards />
+      </ul>
+    </div>
+  );
+};
+
+export default Layout;
